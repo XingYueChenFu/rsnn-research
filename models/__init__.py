20e6c0f69139283144e4d5c63a277b6777bf3774
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2026-04-24 17:23:02 +0800

    Enable RSNN import in models/__init__.py
    Uncommented import for RSNN and RSNNConfig from rsnn_02re.

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+# from .GANN_ver_0_3 import GANN, GANNConfig
+# from .GSNN_ver_0_5 import GSNNGestureModel as GSNN, GSNNConfig
+
+# from .LNN_ver_0_1 import LNN, LiquidTimeConstantCell as LiquidTimeConstantCell
+# from .LNN_ver_0_2 import LNN, LNNConfig, LiquidTimeConstantCell
+
+# from .RSNN_0_2 import RSNN, RSNNConfig
+# from .rsnn_01re import RSNN, RSNNConfig
+from .rsnn_02re import RSNN, RSNNConfig
+
+# from .CRNN_0_1 import CRNN, CRNNConfig
+
+# from .AttentionSNN import *
+
+# from .mamba_0_2 import DVSMambaNet # 需要另行创建环境
+
